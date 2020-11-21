279ffc645ac526a6a95a23d139737c37b8f098e1
Yosh Warrington <yjwarrington@gmail.com>
2020-11-21 21:23:14 +0000

    Initial Commit

---
+++ InventoryService/Controllers/InventoryController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventoryService.Models;
+using InventoryService.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryService.Controllers
+{
+    [Route("v1/")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly IInventoryServices _services;
+
+        public InventoryController(IInventoryServices services)
+        {
+            _services = services;
+        }
+
+        [HttpPost]
+        [Route("AddInventoryItems")]
+        public ActionResult<InventoryItems> AddInventoryItems(InventoryItems items)
+        {
+            
+            var InventoryItems = _services.AddInventoryItems(items);
+            if(InventoryItems == null)
+            {
+                return NotFound();
+            }
+            
+            return InventoryItems;
+
+            //<InventoryItems> retuns that model to this method.
+
+        }
+
+        [HttpGet]
+        [Route("GetInventoryitems")]
+        public ActionResult<Dictionary<string, InventoryItems>> GetInventoryItems()
+        {
+            var inventoryItems = _services.GetInventoryitems();
+
+            if(inventoryItems.Count == 0)
+            {
+                return NotFound();
+            }
+            
+            return inventoryItems;
+        }
+
+    }
+}
